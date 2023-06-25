484d07fc50af2e8144427a5d4a79bc9ed86a73f6
IqbalMlna503 <iqbalmlna.503@gmail.com>
2023-06-25 16:10:53 +0700

    Update DB

--- db-sewa.sql
+++ db-sewa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 05 Jun 2023 pada 08.23
+-- Waktu pembuatan: 25 Jun 2023 pada 11.10
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.1.12
 
@@ -70,9 +70,31 @@ CREATE TABLE `alatoutdoors` (
 --
 
 INSERT INTO `alatoutdoors` (`id_alatoutdoor`, `nama_alat`, `id_kategori`, `spesifikasi`, `deskripsi`, `stok`, `harga_sewa`, `merk`, `image`, `created_at`, `updated_at`) VALUES
-('KD1', 'Sepatu Gunung', 'KT4', 'Everest Catcher', 'Sepatu 123', '100', '15000', 'Vans', '1684442675.sepatu.jpg', '2023-05-18 13:44:35', '2023-05-18 13:44:35'),
-('KD2', 'Tas Carrier', 'KT3', 'Carrier of life', 'Menampung seperti kantong doraemonn', '2000', '20000', 'Paris Hilton', '1684442784.carrier.jpg', '2023-05-18 13:46:24', '2023-05-18 13:46:24'),
-('KD3', 'Sleeping Bag', 'KT2', 'Sleeping like a cocoon', 'Kehangantan seperti pelukan doi :(', '3000', '30000', 'Vindland Sleeping bag', '1684442920.sleepingbag.jpeg', '2023-05-18 13:48:40', '2023-05-18 13:48:40');
+('KD1', 'Sepatu Gunung', 'KT4', 'Everest Catcher', 'Sepatu 123', '97', '15000', 'Vans', '1684442675.sepatu.jpg', '2023-05-18 13:44:35', '2023-06-20 08:27:48'),
+('KD2', 'Tas Carrier', 'KT3', 'Carrier of life', 'Menampung seperti kantong doraemonn', '1997', '20000', 'Paris Hilton', '1684442784.carrier.jpg', '2023-05-18 13:46:24', '2023-06-20 08:27:48'),
+('KD3', 'Sleeping Bag', 'KT2', 'Sleeping like a cocoon', 'Kehangantan seperti pelukan doi :(', '2997', '30000', 'Vindland Sleeping bag', '1684442920.sleepingbag.jpeg', '2023-05-18 13:48:40', '2023-06-20 08:27:48');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `chats`
+--
+
+CREATE TABLE `chats` (
+  `id_chat` varchar(50) NOT NULL,
+  `id_user` varchar(8) DEFAULT NULL,
+  `chat_message` varchar(100) DEFAULT NULL,
+  `status_read` enum('Sudah','Belum') DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `chats`
+--
+
+INSERT INTO `chats` (`id_chat`, `id_user`, `chat_message`, `status_read`, `created_at`, `updated_at`) VALUES
+('CHAT1', 'USR7', 'test', 'Belum', '2023-06-25 09:08:39', '2023-06-25 09:08:39');
 
 -- --------------------------------------------------------
 
@@ -225,8 +247,8 @@ INSERT INTO `pelanggans` (`id_pelanggan`, `id_user`, `nama_pelanggan`, `alamat`,
 ('PLG1', 'USR3', 'rida', NULL, '085749252096', 'perempuan', '2023-04-23 21:26:02', '2023-04-23 21:26:02'),
 ('PLG2', 'USR4', 'Sulis', NULL, NULL, NULL, '2023-04-23 21:28:44', '2023-04-23 21:28:44'),
 ('PLG3', 'USR5', 'deka', NULL, NULL, NULL, '2023-04-24 07:16:29', '2023-04-24 07:16:29'),
-('PLG4', 'USR6', 'Firman', NULL, NULL, NULL, '2023-04-24 07:17:10', '2023-04-24 07:17:10'),
-('PLG5', 'USR7', 'ucup', NULL, NULL, NULL, '2023-05-18 12:42:33', '2023-05-18 12:42:33');
+('PLG4', 'USR6', 'Firman', NULL, '085749252096', NULL, '2023-04-24 07:17:10', '2023-04-24 07:17:10'),
+('PLG5', 'USR7', 'ucup william hutchinson', 'wlkk', '085749252096', 'perempuan', '2023-05-18 12:42:33', '2023-05-18 12:42:33');
 
 -- --------------------------------------------------------
 
@@ -333,7 +355,7 @@ INSERT INTO `users` (`id_user`, `username`, `email`, `email_verified_at`, `passw
 ('USR4', 'sulisrhyu', 'sulisrahayu@gmail.com', NULL, '$2y$10$Nw5Sgu/3Mjhao0NQ0NUn..557q7s7FsXamTflAD1cfS9T8WVU7gMq', 'pelanggan', NULL, '2023-04-23 21:28:44', '2023-04-23 21:28:44'),
 ('USR5', 'dekapramesta', 'dekapramesta@gmail.com', NULL, '$2y$10$BE0LPI7tzxh4LpSZQaipsutDCcjqc83iuGDu.VWjBLDywAUTVYYP2', 'pelanggan', NULL, '2023-04-24 07:16:29', '2023-04-24 07:16:29'),
 ('USR6', 'firmanadi', 'firmanadi@gmail.com', NULL, '$2y$10$G2Mfnc/m8QAPzBTWBw7/yewSkDlVTm8ql9IZF8swGVRrk3k5z1t2C', 'pelanggan', NULL, '2023-04-24 07:17:10', '2023-04-24 07:17:10'),
-('USR7', 'ucup99', 'ucup@email.com', NULL, '$2y$10$fMaN1x9GmWX6UjocdeUhx.yIqAT0j9Gl2.nJxUOwLb5TzrvVPMz8.', 'pelanggan', NULL, '2023-05-18 12:42:33', '2023-05-18 12:42:33');
+('USR7', 'ucup99', 'ucup@email.com', NULL, '$2y$10$fMaN1x9GmWX6UjocdeUhx.yIqAT0j9Gl2.nJxUOwLb5TzrvVPMz8.', 'admin', NULL, '2023-05-18 12:42:33', '2023-05-18 12:42:33');
 
 -- --------------------------------------------------------
 
@@ -348,6 +370,13 @@ CREATE TABLE `user_comments` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user_comments`
+--
+
+INSERT INTO `user_comments` (`id_comment`, `id_pelanggan`, `message_comment`, `created_at`, `updated_at`) VALUES
+('CMNT1', 'PLG5', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2023-06-11 03:06:04', '2023-06-11 03:06:04');
 
 --
 -- Indexes for dumped tables
@@ -366,6 +395,12 @@ ALTER TABLE `admins`
 ALTER TABLE `alatoutdoors`
   ADD PRIMARY KEY (`id_alatoutdoor`),
   ADD KEY `alatoutdoors_id_kategori_foreign` (`id_kategori`);
+
+--
+-- Indeks untuk tabel `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id_chat`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
